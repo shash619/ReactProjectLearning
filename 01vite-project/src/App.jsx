5358f485af8fece2495dd0c062fa2e9c0721a147
shashwat <shashwatmishra1254@gmail.com>
2024-01-04 07:30:30 -0800

    ReactNewProject

--- 01vite-project/src/App.jsx
+++ 01vite-project/src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
 import Chai from "./Chai"
 
 function App() {
+ 
+  let [count,setcounter]=useState(0)
+  function addnumber(){
+    if(count!==20)
+    {
+    count = count+1;
+    setcounter(count)
+    }
+  }
+  const reduceNumber=()=>{
+    if(count!==0){
+    count = count-1;
+    setcounter(count)
+    }
+  }
 
   return (
     <>
     <Chai/>
+    <h2>the value of Count is {count}</h2>
+    <button onClick={addnumber}>Add Counter {count}</button>
+    <br/>
+    <button onClick={reduceNumber}>Reduce Counter</button>
   </>
   )
 }
--- 01vite-project/src/main.jsx
+++ 01vite-project/src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
+const NewElment =React.createElement(
+'a',
+{href:"www.google.com",target:'_blank'},
+"click me to go to the google"
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
